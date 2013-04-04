ebbfe3071360a5f6eac4530f301f2cbd4452a3ba
Michael Comella <michael.l.comella@gmail.com>
2013-04-04 19:11:55 -0400

    Override default app configuration with envvar.

--- mongows/__init__.py
+++ mongows/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('mongows.config.default_settings')
 
+from mongows.config import config_from_envvar
 import mongows.views
+
+config_from_envvar(app)
--- mongows/config/default_settings.py
+++ mongows/config/default_settings.py
@@ -3,13 +3,11 @@ This module defines constants that are intended to be imported into the Flask
 app configuration by default.
 
 """
-import os
-
 # flask.config settings.
 DEBUG = True
 
 # Misc settings.
 HOST = '0.0.0.0'
-PORT = int(os.environ.get('PORT', 5000))
+PORT = 5000
 
-MONGO_URL = os.environ.get('MONGOHQ_URL', 'http://localhost:27017/db')
+MONGO_URL = 'http://localhost:27017/db'
