2caec9410aa82727c7e2cb6d45c4a6637d74832f
Michael Comella <michael.l.comella@gmail.com>
2013-04-04 11:28:27 -0400

    Add settings environment variable for app config.
    If the environment variable is not supplied, execution will continue
    with the default settings. If supplied, the settings at the path given
    by the environment variable will override any given in the default
    settings. If the path is invalid, execution will halt with an error.

--- mongows/__init__.py
+++ mongows/__init__.py
@@ -1,5 +1,11 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 app.config.from_object('mongows.default_settings')
+
+_SETTINGS_ENVVAR = 'MONGOWS_SETTINGS'
+if os.getenv(_SETTINGS_ENVVAR):
+    app.config.from_envvar(_SETTINGS_ENVVAR)
 
 import mongows.views
