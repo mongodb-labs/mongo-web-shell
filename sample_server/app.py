7ca9e5c585432f45eeef0005044e0fc22e70fda4
Michael Comella <michael.l.comella@gmail.com>
2013-04-10 23:37:32 -0400

    Add sample tutorial template.
    This does not yet connect directly with the frontend client code.

--- sample_server/app.py
+++ sample_server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static', static_folder='../frontend')
 
 HOST = '0.0.0.0'
@@ -6,8 +6,8 @@ PORT = 8080
 DEBUG = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def render_tutorial():
+    return render_template('tutorial.html')
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=DEBUG)
