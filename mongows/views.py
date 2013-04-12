1b77e6d60c8a2cde70a4c97f5d941d2c8612e2fd
Michael Comella <michael.l.comella@gmail.com>
2013-04-11 23:50:31 -0400

    Views: Remove session information.
    We don't know how we're handling session code just yet so I've removed
    the loose ends.

--- mongows/views.py
+++ mongows/views.py
@@ -1,4 +1,4 @@
-from flask import request, session
+from flask import request
 
 from mongows import app
 from mongows.db import get_connection
@@ -11,25 +11,16 @@ def hello():
 
 @app.route('/db', methods=['POST'])
 def db_post():
-    db = get_connection()
-    id = session.get('id')
-    if id == None:
-        id = generateId()
     # TODO: create new collection with id
     return '/db Not Implemented'
 
 @app.route('/db/:id', methods=['POST'])
 def db_id_post():
-    id = session.get('id')
-    if id == None:
-        # TODO: return session error
-        pass
     # TODO: call mongo and return results
     query = request.form['query']
     return '/db/:id Not Implemented'
 
 @app.route('/db/:id/keep-alive', methods=['POST'])
 def db_id_keep-alive():
-    id = session.get('id')
     # TODO: call mongo to keep session alive
     return 'keep-alive Not Implemented'
