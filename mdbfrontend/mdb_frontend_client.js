512f155ddcf02202056478a9df85fedad66ea5f2
Lucas Morduchowicz <lucas_morduchowicz@brown.edu>
2013-04-12 17:09:01 -0400

    finished changes in response to comments

--- mdbfrontend/mdb_frontend_client.js
+++ mdbfrontend/mdb_frontend_client.js
@@ -1,68 +1,77 @@
-var CSSPATH = "shellCss.css";
+var KEYCODE = 13;
+var CSSPATH = "shellCss.css"
 
 function insertShells(){
-    //create shells and dbs
-    $(".mongoshell").each(function(){
-        var div = this;
-        $.post("db", null, createMongoShell(div), "text"); //TODO: handle error
-    })
+  //jquery
+  var script = document.createElement('script');
+  script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
+  script.type = 'text/javascript';
+  document.getElementsByTagName('head')[0].appendChild(script);
 
-    //add input listening
-    var inputs = document.getElementsByClassName("sinput");
-    for (var i = 0; i < inputs.length; i++){
-        addInputSubmitEvent(inputs[i]);
-		inputs[i].value = "$";
-    }
+  //create shells and dbs
+  $(".mongoshell").each(function(){
+    var div = this;
+    $.post("db", null, createMongoShell(div), "text");
+  })
 
-    //add the css
-    var element = document.createElement('link');
-    element.href = CSSPATH;
-    element.rel = 'stylesheet';
-    element.type = 'text/css';
-    document.body.appendChild(element);
+  //add input listening
+  var inputs = document.getElementsByClassName("sinput");
+  for (var i = 0; i < inputs.length; i++){
+    addInputSubmitEvent(inputs[i]);
+    inputs[i].value = "$";
+  }
+
+  //add the css
+  var element = document.createElement('link');
+  element.href = CSSPATH;
+  element.rel = 'stylesheet';
+  element.type = 'text/css';
+  document.body.appendChild(element);
 }
 
 function createMongoShell(div){
-    return function(data, status){
-        if (status == 200){ //TODO: case where status != 200
-			var text =  '<div class = "mshell-border"><div class = "mshell-response"><ul class = "in-shell-response" id ="' + data + '" > </ul></div><div class = "shell-input-wrapper"><input type = "text" id ="' + data + '" class = "sinput"></div></div>';
-            div.innerHTML = text;
-        }
+  return function(data, status){
+    if (status == 200){
+      var text =  '<div id = "mshellborder"><div id = "mshell"> <ul class = "inshellresponse" id ="' + data + '"> </ul><input type = "text" id ="' + data + '"class = "sinput"></div>';
+      div.innerHTML = text;
     }
+  }
 }
 
 function addInputSubmitEvent(input) {
-    input.onkeydown = function(e) {
-        e = e || window.event;
-        if (e.keyCode == 13) {
-            submitInputAjax(input);
-        }
-    };
+  input.onkeydown = function(e) {
+    if (e.keyCode == KEYCODE) {
+      submitInputAjax(input);
+    }
+  };
 }
 
 function submitInputAjax(input){
-    var url = "db/:" + input.id + "/find/"
-    var indata = parseInput(input.value);
-    $.post(url, indata, submitHelper(input), "JSON");
-	input.value = "$";
+  var url = "db/:" + input.id + "/find/"
+  var indata = parseInput(input.value);
+  $.post(url, indata, submitHelper(input), "text");
+  input.value = "$";
 }
 
 function submitHelper(input){
-	return function(data.uri, status){
-		if (status == 200){ //TODO: case where status != 200
-            var outputs = document.getElementsByClassName("inshellresponse");
-            for (var output = 0; ouput < outputs.length; output++){
-                if (outputs[output].id.valueOf() == input.id.valueOf()) {
-                    var responseLines = data.split("\n");
-                    for (var line = 0; line < responseLines.length -1; line++){
-                        var newLI = document.createElement("li");
-                        newLI.innerHTML = responseLines[line];
-                        outputs[output].innerHTML.appendChild(newLI);
-                    }
-                }
+  return function(data, status){
+    if (status == 200){
+      var outputs = document.getElementsByClassName("inshellresponse");
+      for (var output in outputs){
+        if (outputs[output].id.valueOf() == input.id.valueOf()) {
+          var responseLines = data.split("\n");
+            for (var line in responseLines){
+              if (line == responseLines.length - 1){
+                break;
+              }
+              var newLI = document.createElement("li");
+              newLI.innerHTML = responseLines[line];
+              outputs[output].innerHTML.appendChild(newLI);
             }
         }
-	};
+      }
+    }
+  };
 }
 
 function insertShellsMock(){
