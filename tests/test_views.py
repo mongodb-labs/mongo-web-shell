a08e7caf82d1b3cb55b5fd1027ef40a900a487d3
Michael Comella <michael.l.comella@gmail.com>
2013-04-15 23:02:57 -0400

    Add CORS support to web service.
    This was largely done through the crossdomain decorator taken from the
    Flask snippet website. The client currently requests json so the server
    has to return valid json; the server view methods (and tests) were
    changed accordingly.

--- tests/test_views.py
+++ tests/test_views.py
@@ -12,19 +12,19 @@ class ViewsTestCase(MongoWSTestCase):
     def test_create_mws_resource(self):
         url = '/mws'
         rv = self.app.post(url)
-        self.assertTrue('Not yet implemented' in rv.data)
+        self.assertTrue('{}' in rv.data)
 
     def test_keep_mws_alive(self):
         url = '/mws/res_id/keep-alive'
         rv = self.app.post(url)
-        self.assertTrue('Not yet implemented' in rv.data)
+        self.assertTrue('{}' in rv.data)
 
     def test_db_collection_find(self):
         url = '/mws/res_id/db/collection_name/find'
         rv = self.app.get(url)
-        self.assertTrue('Not yet implemented' in rv.data)
+        self.assertTrue('{}' in rv.data)
 
     def test_db_collection_insert(self):
         url = '/mws/res_id/db/collection_name/insert'
         rv = self.app.post(url)
-        self.assertTrue('Not yet implemented' in rv.data)
+        self.assertTrue('{}' in rv.data)
