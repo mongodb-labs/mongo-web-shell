08fa7dbf6f48ccc9a65b6f3340de9d0701a63ec4
Sameer Bandal <sameerbandal@gmail.com>
2013-04-16 18:45:15 -0400

    Adding the insert and find function definition
    Restructering the code a bit. Unit tests are yet to be added.

--- mongows/db.py
+++ mongows/db.py
@@ -4,16 +4,44 @@ import pymongo
 
 from mongows import app
 
-db = None
+client = None
 
 def get_connection():
-    global db
-    if db:
-        return db
+    global client
+    if client:
+        return client
     config = urlparse(app.config['MONGO_URL'])
     db_name = config.path.rpartition('/')[2]
-    connection = pymongo.MongoClient(config.hostname, config.port)
-    db = connection[db_name]
-    if config.username:
-        db.authenticate(config.username, config.password)
-    return db
+    client = pymongo.MongoClient(config.hostname, config.port)
+    return client
+
+def collection_find(res_id, collection, arguments):
+    client = get_connection()
+
+    if res_id in mongo_client:
+        db = mongo_client[res_id]
+    else:
+        # TODO: Throw an exception
+        pass
+    if not collection in db:
+        # TODO: Throw an exception
+        pass
+
+    db = client[res_id]
+    mongo_cursor = db[collection].find(arguments)
+
+    return mongo_cursor
+
+def collection_insert(res_id, collection, document):
+    client = get_connection()
+
+    if res_id in mongo_client:
+        db = mongo_client[res_id]
+    else:
+        # TODO: Throw an exception
+        pass
+    if not collection in db:
+        # TODO: Throw an exception
+        pass
+
+    return db[collection].insert(document)
--- mongows/views.py
+++ mongows/views.py
@@ -1,9 +1,10 @@
 from datetime import timedelta
-from flask import current_app, make_response, request
 from functools import update_wrapper
 
-from mongows import app
-from mongows.db import get_connection
+from bson.json_util import dumps, loads
+from flask import current_app, make_response, request
+
+from mongows import app, db
 
 REQUEST_ORIGIN = '*' # TODO: Get this value from app config.
 
@@ -52,15 +53,21 @@ def crossdomain(origin=None, methods=None, headers=None,
 
 @app.route('/')
 def hello():
-    db = get_connection()
+    db_name = config.path.rpartition('/')[2]
+    mongo_client = db.get_connection()
+    db = mongo_client[db_name]
     emptyset = db.some_collection.find()
     return 'Hello World! {0}'.format(emptyset.count())
 
 @app.route('/mws', methods=['POST'])
 @crossdomain(origin=REQUEST_ORIGIN)
 def create_mws_resource():
-    # TODO: Create resource and return its associated id.
-    return '{}'
+    # For now, the rest of the code uses resource ID as the name of the
+    # database to be queried. So I am hardconding it to test. It is yet to be
+    # decided how and where to maintain the relationship between the user,
+    # her resource ID and the database she can query.
+    result = {'res_id': 'test'}
+    return dumps(result)
 
 @app.route('/mws/<res_id>/keep-alive', methods=['POST'])
 @crossdomain(origin=REQUEST_ORIGIN)
@@ -71,11 +78,33 @@ def keep_mws_alive(res_id):
 @app.route('/mws/<res_id>/db/<collection_name>/find', methods=['GET'])
 @crossdomain(origin=REQUEST_ORIGIN)
 def db_collection_find(res_id, collection_name):
-    # TODO: Call find() on the specified params and return results.
-    return '{}'
+    if 'arguments' in request.form:
+        try:
+            arguments = loads(request.form['arguments'])
+        except:
+            # TODO: return error to client
+            pass
+    else:
+        arguments = {}
+
+    mongo_cursor = db.collection_find(res_id, collection_name, arguments)
+    # Get the results from the cursor and convert it to JSON format before
+    # returning
+    result = list(mongo_cursor)
+    return dumps(result)
 
 @app.route('/mws/<res_id>/db/<collection_name>/insert', methods=['POST'])
 @crossdomain(origin=REQUEST_ORIGIN)
 def db_collection_insert(res_id, collection_name):
-    # TODO: Call insert() on the specified params, return status.
-    return '{}'
+    if 'document' in request.form:
+        try:
+            document = loads(request.form['document'])
+        except:
+            # TODO: return error to client
+            pass
+    else:
+        # TODO: return an error. You must have document/s for insert.
+        pass
+
+    result = db.collection_insert(res_id, collection_name, document)
+    return dumps(result)
