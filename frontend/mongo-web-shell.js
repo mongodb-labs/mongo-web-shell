7bd193e4db1c341d4384627e67bfa694a5e70147
Michael Comella <michael.l.comella@gmail.com>
2013-04-18 20:42:54 -0400

    Move frontend code into submodules.
    In order to accomodate these changes, the semantics of some functions
    have changed; however, the general structure remains the same.
    
    The config <data> attributes were moved to the <script> tag, however.

--- frontend/mongo-web-shell.js
+++ frontend/mongo-web-shell.js
@@ -1,90 +1,100 @@
-// TODO: Remove devel once we integrate with debug().
 /* jshint camelcase: false, devel: true, unused: false */
-var mongoWebShell = (function () {
-  var CSS_PATH, MWS_BASE_RES_URL, MWS_HOST;
-  // Default values.
-  CSS_PATH = 'mongo-web-shell.css';
-  MWS_HOST = 'http://localhost:5000';
-  MWS_BASE_RES_URL = MWS_HOST + '/mws';
+var mongo = {};
 
-  function updateExternalResourcePaths($shell) {
-    // TODO: Document these data attributes.
-    // TODO: Should each shell be able to have its own host?
-    CSS_PATH = $shell.data('css-path') || CSS_PATH;
-    MWS_HOST = $shell.data('mws-host') || MWS_HOST;
-    MWS_BASE_RES_URL = MWS_HOST + '/mws';
-  }
+/**
+ * Injects a mongo web shell into the DOM wherever an element of class
+ * 'mongo-web-shell' can be found. Additionally sets up the resources
+ * required by the web shell, including the mws REST resource and the mws
+ * CSS stylesheets.
+ */
+mongo.init = function () {
+  var config = mongo.dom.retrieveConfig();
+  mongo.dom.injectStylesheet(config.cssPath);
 
-  function injectStylesheet() {
-    var linkElement = document.createElement('link');
-    linkElement.href = CSS_PATH;
-    linkElement.rel = 'stylesheet';
-    linkElement.type = 'text/css';
-    // TODO: Prepend? This would make the stylesheet overridable.
-    $('head').append(linkElement);
-  }
+  $('.mongo-web-shell').each(function (index, shellElement) {
+    var $shell = $(shellElement);
+    mongo.dom.injectShellHTML($shell);
 
-  function injectShellHTML($element) {
-    // TODO: Use client-side templating instead.
-    // TODO: Why is there a border class? Can it be done with CSS border (or be
-    // renamed to be more descriptive)?
-    // TODO: .mshell not defined in CSS; change it.
-    var html = '<div class="mws-border">' +
-                 '<div class="mshell">' +
-                   '<ul class="mws-in-shell-response"></ul>' +
-                   '<form>' +
-                     '<input type="text" class="mws-input" disabled="true">' +
-                   '</form>' +
-                 '</div>' +
-               '</div>';
-    $element.html(html);
-  }
-
-  function handleShellInput(data, mwsResourceID) {
-    console.log('Received text:', data, mwsResourceID);
-    // TODO: Merge #25: Parse <input> content; remove console.log. Make AJAX
-    // request based on parsed input. On success/error, return output to
-    // console, at class mws-in-shell-response.
-  }
-
-  function attachShellInputHandler($shell, mwsResourceID) {
-    $shell.find('form').submit(function (e) {
-      var $input;
-      e.preventDefault();
-      $input = $(e.target).find('.mws-input');
-      handleShellInput($input.val(), mwsResourceID);
-      $input.val('');
+    // Create MWS resource on remote server.
+    $.post(config.baseUrl, null, function (data, textStatus, jqXHR) {
+      if (!data.res_id) {
+        // TODO: Print error in shell. Improve error below.
+        console.warn('No res_id received! Shell disabled.', data);
+        //return;
+      }
+      console.info('/mws/' + data.res_id, 'was created succssfully.');
+      mongo.dom.attachShellInputHandler($shell, data.res_id);
+      $shell.find('.mws-input').get(0).disabled = false;
+    },'json').fail(function (jqXHR, textStatus, errorThrown) {
+      // TODO: Display error message in the mongo web shell.
+      console.error('AJAX request failed:', textStatus, errorThrown);
     });
-  }
+  });
+};
+
+mongo.dom = (function () {
+  // TODO: Document these data attributes.
+  // TODO: Should each shell be able to have its own host?
+  // Default config values.
+  var CSS_PATH = 'mongo-web-shell.css';
+  var MWS_HOST = 'http://localhost:5000';
+  var BASE_URL = MWS_HOST + '/mws';
 
   return {
-    /**
-     * Injects a mongo web shell into the DOM wherever an element of class
-     * 'mongo-web-shell' can be found. Additionally sets up the resources
-     * required by the web shell, including the mws REST resource and the mws
-     * CSS stylesheets.
-     */
-    injectShells: function () {
-      $('.mongo-web-shell').each(function (index, shellElement) {
-        var $shell = $(shellElement);
-        updateExternalResourcePaths($shell);
-        injectShellHTML($shell);
-        $.post(MWS_BASE_RES_URL, null, function (data, textStatus, jqXHR) {
-          if (!data.res_id) {
-            // TODO: Print error in shell. Improve error below.
-            console.log('No res_id received!', data);
-            return;
-          }
-          attachShellInputHandler($shell, data.res_id);
-          $shell.find('.mws-input')[0].disabled = false;
-        }, 'json').fail(function (jqXHR, textStatus, errorThrown) {
-          // TODO: Display error message in the mongo web shell. Remove log.
-          console.log('AJAX request failed:', textStatus, errorThrown);
-        });
+    retrieveConfig: function () {
+      var $curScript = $('script').last();
+      return {
+        cssPath: $curScript.data('css-path') || CSS_PATH,
+        mwsHost: $curScript.data('mws-host') || MWS_HOST,
+        baseUrl: MWS_HOST + '/mws'
+      };
+    },
+
+    injectStylesheet: function (cssPath) {
+      var linkElement = document.createElement('link');
+      linkElement.href = cssPath;
+      linkElement.rel = 'stylesheet';
+      linkElement.type = 'text/css';
+      $('head').prepend(linkElement); // Prepend so css can be overridden.
+    },
+
+    injectShellHTML: function ($element) {
+      // TODO: Use client-side templating instead.
+      // TODO: Why is there a border class? Can it be done with CSS border (or
+      // be renamed to be more descriptive)?
+      // TODO: .mshell not defined in CSS; change it.
+      var html = '<div class="mws-border">' +
+                   '<div class="mshell">' +
+                     '<ul class="mws-in-shell-response"></ul>' +
+                     '<form>' +
+                       '<input type="text" class="mws-input" ' +
+                           'disabled="true">' +
+                     '</form>' +
+                   '</div>' +
+                 '</div>';
+      $element.html(html);
+    },
+
+    attachShellInputHandler: function ($shell, mwsResourceID) {
+      $shell.find('form').submit(function (e) {
+        e.preventDefault();
+        var $input = $(e.target).find('.mws-input');
+        mongo.input.handleShellInput($input.val(), mwsResourceID);
+        $input.val('');
       });
-      injectStylesheet();
     }
   };
 }());
 
-$(document).ready(mongoWebShell.injectShells);
+mongo.input = (function () {
+  return {
+    handleShellInput: function (data, mwsResourceID) {
+      console.debug('Received text:', data, mwsResourceID);
+      // TODO: Merge #25: Parse <input> content; Make AJAX request based on
+      // parsed input. On success/error, return output to console, at class
+      // mws-in-shell-response.
+    }
+  };
+}());
+
+$(document).ready(mongo.init);
