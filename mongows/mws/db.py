f418dd4b3f6cd540dd48410cf813e01d2a5751c2
Michael Comella <michael.l.comella@gmail.com>
2013-05-01 10:18:17 -0400

    Remove collection checks.
    mongo lazily creates databases so you can query on non-existent
    collections just fine.

--- mongows/mws/db.py
+++ mongows/mws/db.py
@@ -32,17 +32,9 @@ def get_db():
 
 def collection_find(res_id, collection, query, projection):
     db = get_db()
-    if not hasattr(db, collection):
-        # TODO: Throw an exception
-        print 'ERROR: Could not find the collection in DB. ' + \
-            'Collection name: ' + collection
     return db[collection].find(query, projection)
 
 
 def collection_insert(res_id, collection, document):
     db = get_db()
-    if not hasattr(db, collection):
-        # TODO: Throw an exception
-        print 'ERROR: Could not find the collection in DB. ' + \
-              'Collection name: ' + collection
     return db[collection].insert(document)
