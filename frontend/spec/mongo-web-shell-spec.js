e37e47304058336d67c7be4e86752d29848b5704
Michael Comella <michael.l.comella@gmail.com>
2013-05-16 10:45:51 -0400

    Add keyword.evaluate test.

--- frontend/spec/mongo-web-shell-spec.js
+++ frontend/spec/mongo-web-shell-spec.js
@@ -434,7 +434,58 @@ describe('The dom module', function () {
 
 
 describe('The keyword module', function () {
-  // TODO: Test.
+  var mk = mongo.keyword;
+
+  it('switches over which keyword function to call', function () {
+    var shellID = 0;
+    var shellSpy = jasmine.createSpyObj('Shell', ['insertResponseLine']);
+    mongo.shells[shellID] = shellSpy;
+    var old;
+    var keywords = ['help', 'it', 'show', 'use'];
+    keywords.forEach(function (keyword) {
+      spyOn(mk, keyword);
+      var args = [];
+      for (var i = 0; i < 3; i++) {
+        mk.evaluate(shellID, keyword, args[0], args[1]);
+        expect(mk[keyword]).toHaveBeenCalledWith(shellSpy, args[0], args[1]);
+        expect(mk[keyword].calls.length).toBe(i + 1);
+        args.push(0);
+      }
+
+      // unusedArg.
+      switch (keyword) {
+      case 'help':
+      case 'show':
+        old = {
+          insert: shellSpy.insertResponseLine.calls.length,
+          keyword: mk[keyword].calls.length
+        };
+        mk.evaluate(shellID, keyword, args[0], args[1], args[2]);
+        expect(shellSpy.insertResponseLine.calls.length).toBe(old.insert + 1);
+        expect(mk[keyword].calls.length).toBe(old.keyword);
+        break;
+
+      case 'it':
+      case 'use':
+        old = mk[keyword].calls.length;
+        mk.evaluate(shellID, keyword, args[0], args[1], args[2]);
+        expect(mk[keyword].calls.length).toBe(old + 1);
+        break;
+      }
+    });
+
+    // Bad keyword.
+    old = {};
+    keywords.forEach(function (keyword) {
+      old[keyword] = mk[keyword].calls.length;
+    });
+    old._insert = shellSpy.insertResponseLine.calls.length;
+    mk.evaluate(shellID, 'bad_keyword');
+    expect(shellSpy.insertResponseLine.calls.length).toBe(old._insert + 1);
+    keywords.forEach(function (keyword) {
+      expect(mk[keyword].calls.length).toBe(old[keyword]);
+    });
+  });
 });
 
 
