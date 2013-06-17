1809c0f616ed657d1f82dff1ef995848a69953f2
Ryan Chan <ryan@rcchan.com>
2013-06-17 18:20:03 -0400

    Move object stringification to util

--- frontend/src/mws/Shell.js
+++ frontend/src/mws/Shell.js
@@ -15,16 +15,7 @@ mongo.Shell = function (rootElement, shellID) {
       shellBatchSize: mongo.const.shellBatchSize
     },
     print: $.proxy(function(expr){
-      if (expr.toString === Object.prototype.toString){
-        try {
-          expr = JSON.stringify(expr);
-          this.insertResponseLine(expr);
-        } catch(e){
-          this.insertResponseLine('ERROR: ' + e.message);
-        }
-      } else {
-        this.insertResponseLine(expr.toString());
-      }
+      this.insertResponseLine(mongo.util.toString(expr));
     }, this)
   };
 };
