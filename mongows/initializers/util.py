d276ff05cc8037722c8abb6e4702a3a2bd5cb2e4
Danny Weinberg <daniel.weinberg@10gen.com>
2013-06-28 18:17:59 -0400

    Add backend utilities for loading data.

---
+++ mongows/initializers/util.py
@@ -0,0 +1,52 @@
+from bson.json_util import loads
+from mongows.mws.db import get_db
+from mongows.mws.util import UseResId
+
+
+def load_data_from_mongoexport(res_id, export_location, collection_name,
+                               remove_id=False):
+    """
+    This file should come from mongoexport, with or without the --jsonArray
+    flag. That is to say, it should either be a series of documents, each on
+    its own line, or a single array of documents. All documents will be
+    inserted into the given collection.
+    """
+    with open(export_location) as export:
+    # export = open(export_location)
+        first_char = export.read(1)
+        export.seek(-1)
+        if first_char == '[':
+            # Data is already in an array
+            documents = loads(export.read())
+        else:
+            # Each line of data is an object
+            documents = []
+            for line in export:
+                documents.append(loads(line))
+        if remove_id:
+            _remove_id(documents)
+
+        with UseResId(res_id):
+            get_db()[collection_name].insert(documents)
+
+
+def load_data_from_json(res_id, file_name, remove_id=False):
+    """
+    The top level of this file should be an object who's keys are collection
+    names which map to an array of documents to be inserted into the collection
+    """
+    with open(file_name) as json_file:
+        collections = loads(json_file.read())
+        db = get_db()
+        with UseResId(res_id):
+            for collection, documents in collections.iteritems():
+                if remove_id:
+                    _remove_id(documents)
+                db[collection].insert(documents)
+
+
+def _remove_id(documents):
+    """ Removes the _id field from each document in the array """
+    for document in documents:
+        if '_id' in document:
+            del document['_id']
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,3 +1,10 @@
+import StringIO
+from bson.json_util import dumps
+import mock
+from mongows.initializers.util import (
+    load_data_from_mongoexport,
+    load_data_from_json,
+)
 from mongows.mws.db import get_db
 from mongows.mws.util import UseResId
 from tests import MongoWSTestCase
@@ -12,3 +19,115 @@ class UseResIdTestCase(MongoWSTestCase):
                 self.assertEqual(coll.name, 'myresid.foo')
             coll = db.foo
             self.assertEqual(coll.name, 'foo')
+
+
+class InitializersTestCase(MongoWSTestCase):
+    def mock_open(self, open_mock, contents):
+        file_wrapper = mock.MagicMock(spec=file)
+        open_mock.return_value = file_wrapper
+        file_handle = StringIO.StringIO(contents)
+        file_wrapper.__enter__.return_value = file_handle
+
+    @mock.patch('__builtin__.open')
+    def test_loads_exported_data(self, open_mock):
+        documents = [
+            {'_id': 1, 'msg': 'my test string'},
+            {'_id': 2, 'message': 'my other string'},
+            {'_id': 3, 'foo': 'bar', 'greeting': 'hi there'},
+        ]
+        file_contents = '\n'.join([dumps(doc) for doc in documents])
+        self.mock_open(open_mock, file_contents)
+
+        with self.real_app.app_context():
+            db = get_db()
+
+            # Test normally (keeping the _id)
+            load_data_from_mongoexport('myresid.', 'my/file/location',
+                                       'mycoll')
+            open_mock.assert_called_with('my/file/location')
+            collection_contents = list(db['myresid.mycoll'].find())
+            self.assertItemsEqual(collection_contents, documents)
+
+            db['myresid.mycoll'].drop()
+
+            # Test removing the _id
+            load_data_from_mongoexport('myresid.', 'my/file/location',
+                                       'mycoll', True)
+            collection_contents = list(db['myresid.mycoll'].find())
+            for doc in collection_contents:
+                # Should not be any of the given _id's
+                self.assertNotIn(doc['_id'], (1, 2, 3))
+            db['myresid.mycoll'].drop()
+
+    @mock.patch('__builtin__.open')
+    def test_loads_exported_array_data(self, open_mock):
+        documents = [
+            {'_id': 1, 'msg': 'my test string'},
+            {'_id': 2, 'message': 'my other string'},
+            {'_id': 3, 'foo': 'bar', 'greeting': 'hi there'},
+        ]
+        file_contents = dumps(documents)
+        self.mock_open(open_mock, file_contents)
+
+        with self.real_app.app_context():
+            db = get_db()
+
+            load_data_from_mongoexport('myresid.',
+                                       'my/file/location', 'mycoll')
+
+            open_mock.assert_called_with('my/file/location')
+            collection_contents = list(db['myresid.mycoll'].find())
+            self.assertItemsEqual(collection_contents, documents)
+
+            db['myresid.mycoll'].drop()
+
+    @mock.patch('__builtin__.open')
+    def test_loads_json_data(self, open_mock):
+        documents = {
+            'first_coll': [
+                {'_id': 1, 'msg': 'my test string'},
+                {'_id': 2, 'message': 'my other string'},
+                {'_id': 3, 'foo': 'bar', 'greeting': 'hi there'},
+            ],
+            'viewing_preferences': [
+                {'_id': 2, 'tv_shows': ['archer', 'firefly']},
+                {'_id': 3, 'tv_shows': ['arrested development'],
+                 'movies': ['shawshank redemption']},
+            ],
+        }
+        file_contents = dumps(documents)
+
+        with self.real_app.app_context():
+            db = get_db()
+
+            # Test normally (keeping the _id)
+            self.mock_open(open_mock, file_contents)
+            load_data_from_json('myresid.', 'my/file/location')
+
+            open_mock.assert_called_with('my/file/location')
+            first_coll_contents = list(db['myresid.first_coll'].find())
+            self.assertItemsEqual(first_coll_contents, documents['first_coll'])
+            viewing_prefs_contents = list(
+                db['myresid.viewing_preferences'].find()
+            )
+            self.assertItemsEqual(viewing_prefs_contents,
+                                  documents['viewing_preferences'])
+
+            db['myresid.first_coll'].drop()
+            db['myresid.viewing_preferences'].drop()
+
+            # Test removing the _id's
+            self.mock_open(open_mock, file_contents)
+            load_data_from_json('myresid.', 'my/file/location', True)
+
+            first_coll_contents = list(db['myresid.first_coll'].find())
+            for doc in first_coll_contents:
+                self.assertNotIn(doc['_id'], (1, 2, 3))
+            viewing_prefs_contents = list(
+                db['myresid.viewing_preferences'].find()
+            )
+            for doc in viewing_prefs_contents:
+                self.assertNotIn(doc['_id'], (1, 2, 3))
+
+            db['myresid.first_coll'].drop()
+            db['myresid.viewing_preferences'].drop()
