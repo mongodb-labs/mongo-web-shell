a7134612fc9592b9e674cd7c5012a067b449b5c4
Ryan Chan <ryan@rcchan.com>
2013-07-08 12:08:42 -0400

    Validation on _id

--- mongows/validators/ValidationTest.py
+++ mongows/validators/ValidationTest.py
@@ -18,28 +18,31 @@ class ValidationTest:
         return tuple(d.items())
 
     # Collection must exactly equal the data set
-    def collection_equals(self, collection, data):
+    def collection_equals(self, collection, data, check_id=False):
         data = [self.__hashDict(x) for x in data]
         with UseResId(self.res_id):
-            result = list(self.db[collection].find({}, {'_id': 0}))
+            projection = None if check_id else {'_id': 0}
+            result = list(self.db[collection].find({}, projection))
             result = [self.__hashDict(x) for x in result]
             return Counter(result) == Counter(data)
 
     # Data must be a subset of collection
-    def collection_contains(self, collection, data):
+    def collection_contains(self, collection, data, check_id=False):
         with UseResId(self.res_id):
-            result = list(self.db[collection].find({'$or': data}, {'_id': 0}))
+            projection = None if check_id else {'_id': 0}
+            result = list(self.db[collection].find({'$or': data}, projection))
             return all(x in result for x in data)
 
     # Collection must contain one or more of the elements in data
-    def collection_contains_any(self, collection, data):
+    def collection_contains_any(self, collection, data, check_id=False):
         with UseResId(self.res_id):
-            result = list(self.db[collection].find({'$or': data}, {'_id': 0}))
+            projection = None if check_id else {'_id': 0}
+            result = list(self.db[collection].find({'$or': data}, projection))
             return any(x in result for x in data)
 
     # Collection does not contain any of the elements in data
-    def collection_contains_none(self, collection, data):
-        return not self.collection_contains_any(collection, data)
+    def collection_contains_none(self, collection, data, check_id=False):
+        return not self.collection_contains_any(collection, data, check_id)
 
     # Require all inheriting classes to implement a run method
     @abstractmethod
--- tests/test_validation_utils.py
+++ tests/test_validation_utils.py
@@ -17,7 +17,9 @@ class ValidationUtilsTestCase(MongoWSTestCase):
     def tearDown(self):
         self.db.drop_collection(self.coll)
 
-    def test_collection_equals(self):
+
+class CollectionEqualsUnitTest(ValidationUtilsTestCase):
+    def test_equals(self):
         self.assertTrue(self.validator.collection_equals('coll', []))
         self.assertFalse(self.validator.collection_equals('coll', [{'a': 1}]))
         for i in range(3):
@@ -26,7 +28,20 @@ class ValidationUtilsTestCase(MongoWSTestCase):
             {'a': 0}, {'a': 1}, {'a': 2}]))
         self.assertFalse(self.validator.collection_equals('coll', [{'a': 1}]))
 
-    def test_collection_contains(self):
+    def test_equals_id(self):
+        self.assertTrue(self.validator.collection_equals('coll', [], True))
+        for i in range(3):
+            self.db.test_coll.insert({'_id': 'id%s' % i, 'a': i})
+        self.assertFalse(self.validator.collection_equals('coll', [
+            {'a': 0}, {'a': 1}, {'a': 2}], True))
+        self.assertTrue(self.validator.collection_equals('coll', [
+            {'_id': 'id0', 'a': 0},
+            {'_id': 'id1', 'a': 1},
+            {'_id': 'id2', 'a': 2}], True))
+
+
+class CollectionContainsUnitTest(ValidationUtilsTestCase):
+    def test_contains(self):
         for i in range(3):
             self.db.test_coll.insert({'a': i})
         self.assertTrue(self.validator.collection_contains('coll',
@@ -38,7 +53,19 @@ class ValidationUtilsTestCase(MongoWSTestCase):
                                                             [{'a': 1},
                                                              {'b': 2}]))
 
-    def test_collection_contains_any(self):
+    def test_contains_id(self):
+        for i in range(3):
+            self.db.test_coll.insert({'_id': 'id%s' % i, 'a': i})
+        self.assertFalse(self.validator.collection_contains('coll',
+                         [{'a': 1}], True))
+        self.assertTrue(self.validator.collection_contains('coll', [
+            {'_id': 'id1', 'a': 1}], True))
+        self.assertFalse(self.validator.collection_contains('coll', [
+            {'_id': 'id1', 'a': 1}, {'_id': 'id2', 'b': 2}]), True)
+
+
+class CollectionContainsAnyUnitTest(ValidationUtilsTestCase):
+    def test_contains_any(self):
         for i in range(3):
             self.db.test_coll.insert({'a': i})
         self.assertTrue(self.validator.collection_contains_any('coll',
@@ -51,13 +78,21 @@ class ValidationUtilsTestCase(MongoWSTestCase):
                                                                [{'a': 1},
                                                                {'b': 2}]))
 
-    def test_collection_contains_none(self):
+    def test_contains_any_id(self):
+        for i in range(3):
+            self.db.test_coll.insert({'_id': 'id%s' % i, 'a': i})
+        self.assertFalse(self.validator.collection_contains_any('coll',
+                         [{'a': 1}], True))
+        self.assertTrue(self.validator.collection_contains_any('coll', [
+            {'_id': 'id1', 'a': 1}], True))
+        self.assertTrue(self.validator.collection_contains_any('coll', [
+            {'_id': 'id1', 'a': 1}, {'_id': 'id2', 'b': 2}], True))
+
+
+def CollectionContainsNoneUnitTest(ValidationUtilsTestCase):
+    def test_contains_none(self):
         for i in range(3):
             self.db.test_coll.insert({'a': i})
-        self.assertTrue(self.validator.collection_contains_any('coll',
-                                                               [{'a': 0},
-                                                                {'a': 1},
-                                                                {'a': 2}]))
         self.assertFalse(self.validator.collection_contains_none('coll',
                                                                  [{'a': 1}]))
         self.assertFalse(self.validator.collection_contains_none('coll',
@@ -65,3 +100,15 @@ class ValidationUtilsTestCase(MongoWSTestCase):
                                                                   {'b': 2}]))
         self.assertTrue(self.validator.collection_contains_none('coll',
                                                                 [{'b': 2}]))
+
+    def test_contains_none_id(self):
+        for i in range(3):
+            self.db.test_coll.insert({'a': i})
+        self.assertTrue(self.validator.collection_contains_none('coll',
+                                                                [{'a': 1}],
+                                                                True))
+        self.assertFalse(self.validator.collection_contains_none('coll', [
+                         {'_id': 'id1', 'a': 1}]))
+        self.assertTrue(self.validator.collection_contains_none('coll',
+                                                                [{'_id': 'id2',
+                                                                  'b': 2}]))
