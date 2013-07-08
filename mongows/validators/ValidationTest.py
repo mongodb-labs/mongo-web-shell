0fd42ffbd777a5ca47afb97a6dbb52772b2fa329
Ryan Chan <ryan@rcchan.com>
2013-07-08 11:20:37 -0400

    Better way of checking collection equality for validation

--- mongows/validators/ValidationTest.py
+++ mongows/validators/ValidationTest.py
@@ -1,6 +1,7 @@
 from mongows.mws.db import get_db
 from mongows.mws.util import UseResId
 from abc import ABCMeta, abstractmethod
+from collections import Counter
 
 
 class ValidationTest:
@@ -10,11 +11,19 @@ class ValidationTest:
         self.res_id = res_id
         self.db = get_db()
 
+    def __hashDict(self, d):
+        for key in d:
+            if isinstance(d[key], dict):
+                d[key] = __hashDict(d[key])
+        return tuple(d.items())
+
     # Collection must exactly equal the data set
     def collection_equals(self, collection, data):
+        data = [self.__hashDict(x) for x in data]
         with UseResId(self.res_id):
             result = list(self.db[collection].find({}, {'_id': 0}))
-            return sorted(result) == sorted(data)
+            result = [self.__hashDict(x) for x in result]
+            return Counter(result) == Counter(data)
 
     # Data must be a subset of collection
     def collection_contains(self, collection, data):
