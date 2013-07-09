71e620288866b990bd12d5ee9d03f324b586df9d
Ryan Chan <ryan@rcchan.com>
2013-07-09 13:56:43 -0400

    simplify validation util code

--- mongows/validators/ValidationTest.py
+++ mongows/validators/ValidationTest.py
@@ -17,36 +17,40 @@ class ValidationTest:
                 d[key] = __hashDict(d[key])
         return tuple(d.items())
 
+    def __precompute(self, collection, data_only, data, check_id):
+        with UseResId(self.res_id):
+            query = {'$or': data} if data_only else {}
+            projection = None if check_id else {'_id': 0}
+            result = self.db[collection].find(query, projection)
+            data = (self.__hashDict(x) for x in data)
+            result = (self.__hashDict(x) for x in result)
+            return data, result
+
     # Collection must exactly equal the data set
     def collection_equals(self, collection, data, check_id=False):
-        data = (self.__hashDict(x) for x in data)
-        with UseResId(self.res_id):
-            projection = None if check_id else {'_id': 0}
-            result = self.db[collection].find({}, projection)
-            result = (self.__hashDict(x) for x in result)
-            return Counter(result) == Counter(data)
+        data, result = self.__precompute(collection, False, data, check_id)
+        return Counter(result) == Counter(data)
 
     # Data must be a subset of collection
     def collection_contains(self, collection, data, check_id=False):
-        with UseResId(self.res_id):
-            projection = None if check_id else {'_id': 0}
-            result = list(self.db[collection].find({'$or': data}, projection))
-            data = Counter(self.__hashDict(x) for x in data)
-            result = Counter(self.__hashDict(x) for x in result)
-            return all(data[key] <= result[key] for key in data)
+        data, result = self.__precompute(collection, True, data, check_id)
+        data = Counter(data)
+        result = Counter(result)
+        return all(data[key] <= result[key] for key in data)
 
     # Collection must contain one or more of the elements in data
     def collection_contains_any(self, collection, data, check_id=False):
-        with UseResId(self.res_id):
-            projection = None if check_id else {'_id': 0}
-            result = list(self.db[collection].find({'$or': data}, projection))
-            data = {self.__hashDict(x) for x in data}
-            result = {self.__hashDict(x) for x in result}
-            return any(x in result for x in data)
+        data, result = self.__precompute(collection, True, data, check_id)
+        data = set(data)
+        result = set(result)
+        return any(x in result for x in data)
 
     # Collection does not contain any of the elements in data
     def collection_contains_none(self, collection, data, check_id=False):
-        return not self.collection_contains_any(collection, data, check_id)
+        data, result = self.__precompute(collection, True, data, check_id)
+        data = set(data)
+        result = set(result)
+        return all(x not in result for x in data)
 
     # Require all inheriting classes to implement a run method
     @abstractmethod
