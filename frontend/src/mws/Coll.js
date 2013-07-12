33a4d703dcf92c833e02e2e0f52d56428b5935b1
Ryan Chan <ryan@rcchan.com>
2013-07-12 16:28:46 -0400

    Pass collection name to events

--- frontend/src/mws/Coll.js
+++ frontend/src/mws/Coll.js
@@ -34,7 +34,8 @@ mongo.Coll.prototype.toString = function () {
  * server.
  */
 mongo.Coll.prototype.find = function (query, projection) {
-  mongo.events.functionTrigger(this.shell, 'db.collection.find', arguments);
+  mongo.events.functionTrigger(this.shell, 'db.collection.find', arguments,
+                               {collection: this.name});
   return new mongo.Cursor(this, query, projection);
 };
 
@@ -50,7 +51,8 @@ mongo.Coll.prototype.findOne = function (query, projection) {
 mongo.Coll.prototype.insert = function (doc) {
   var url = this.urlBase + 'insert';
   var params = {document: doc};
-  mongo.events.functionTrigger(this.shell, 'db.collection.insert', arguments);
+  mongo.events.functionTrigger(this.shell, 'db.collection.insert', arguments,
+                               {collection: this.name});
   mongo.request.makeRequest(url, params, 'POST', 'dbCollectionInsert', this.shell);
 };
 
@@ -62,7 +64,8 @@ mongo.Coll.prototype.insert = function (doc) {
 mongo.Coll.prototype.remove = function (constraint, justOne) {
   var url = this.urlBase + 'remove';
   var params = {constraint: constraint, just_one: justOne};
-  mongo.events.functionTrigger(this.shell, 'db.collection.remove', arguments);
+  mongo.events.functionTrigger(this.shell, 'db.collection.remove', arguments,
+                               {collection: this.name});
   mongo.request.makeRequest(url, params, 'DELETE', 'dbCollectionRemove', this.shell);
 };
 
@@ -77,7 +80,8 @@ mongo.Coll.prototype.remove = function (constraint, justOne) {
  */
 mongo.Coll.prototype.update = function (query, update, upsert, multi) {
   var url = this.urlBase + 'update';
-  mongo.events.functionTrigger(this.shell, 'db.collection.update', arguments);
+  mongo.events.functionTrigger(this.shell, 'db.collection.update', arguments,
+                               {collection: this.name});
 
   // handle options document for 2.2+
   if (typeof upsert === 'object'){
@@ -102,7 +106,8 @@ mongo.Coll.prototype.update = function (query, update, upsert, multi) {
  */
 mongo.Coll.prototype.drop = function () {
   var url = this.urlBase + 'drop';
-  mongo.events.functionTrigger(this.shell, 'db.collection.drop', arguments);
+  mongo.events.functionTrigger(this.shell, 'db.collection.drop', arguments,
+                               {collection: this.name});
   mongo.request.makeRequest(url, null, 'DELETE', 'dbCollectionDrop', this.shell);
 };
 
@@ -119,7 +124,8 @@ mongo.Coll.prototype.aggregate = function(query){
     results = data;
   }.bind(this);
 
-  mongo.events.functionTrigger(this.shell, 'db.collection.aggregate', arguments);
+  mongo.events.functionTrigger(this.shell, 'db.collection.aggregate', arguments,
+                               {collection: this.name});
   mongo.request.makeRequest(url, query, 'GET', 'dbCollectionAggregate', this.shell,
                             onSuccess, false); // Sync request, blocking
   return results;
