9b8e1c2b5c83c65f2375c075e3e0428278216519
Ryan Chan <ryan@rcchan.com>
2013-07-16 14:13:45 -0400

    Split jQuery init to separate files

--- frontend/src/mws/init.js
+++ frontend/src/mws/init.js
@@ -142,7 +142,7 @@ mongo.init = (function(){
   };
 
   var run = function () {
-    mongo.init.jQuery(jQuery);
+    mongo.jQueryInit(jQuery);
     mongo.util.enableConsoleProtection();
     var config = mongo.config = mongo.dom.retrieveConfig();
     mongo.dom.injectStylesheet(config.cssPath);
@@ -150,7 +150,7 @@ mongo.init = (function(){
 
     // Request a resource ID, give it to all the shells, and keep it alive
     mongo.request.createMWSResource(mongo.shells, function (data) {
-      mongo.init._res_id = data.res_id;
+      mongo.init.res_id = data.res_id;
 
       setInterval(
         function () { mongo.request.keepAlive(data.res_id); },
@@ -173,110 +173,13 @@ mongo.init = (function(){
     callback();
   };
 
-  // set up $.mws
-  var jQueryInit = function($){
-    $.fn.extend({
-      mws: function(options){
-        switch (typeof options){
-        case 'string':
-          // manipulate properties of existing shell
-          switch (options){
-          case 'lock':
-            this.each(function(i, e){ $(e).data('shell').enableInput(false); });
-            break;
-
-          case 'unlock':
-            this.each(function(i, e){ $(e).data('shell').enableInput(true); });
-            break;
-
-          case 'width':
-            this.width(arguments[1]);
-            break;
-
-          case 'height':
-            this.height(arguments[1]);
-            break;
-
-          case 'loadUrl':
-            // loads data from url into first selected shell
-            mongo.init._initShell(this[0], mongo.init._res_id, {
-              create_new: false,
-              init_data: true,
-              init_url: arguments[1]
-            });
-            break;
-
-          case 'loadJSON':
-            // loads data from json into first selected shell
-            mongo.init._initShell(this[0], mongo.init._res_id, {
-              create_new: false,
-              init_data: true,
-              init_json: arguments[1]
-            });
-            break;
-
-          case 'input':
-            var val = arguments[1];
-            this.each(function(i, e){
-              $(e).data('shell').$input.val(val);
-            });
-            break;
-
-          case 'submit':
-            this.each(function(i, e){ $(e).data('shell').handleInput(); });
-            break;
-          }
-          break;
-
-        case 'object':
-        case 'undefined': // accept no parameters as call to default constructor
-          this.addClass('mongo-web-shell').each(function(i, e){
-            // determine the initialization options for the current shell
-            // preferring undefined over null prevents null properties from overwriting defaults
-            var opt = $.extend({}, $.mws.defaults, {
-              init_url: $(e).data('initialization-url') || undefined,
-              init_json: $(e).data('initialization-json') || undefined
-            }, options);
-
-            mongo.init._initShell(e, mongo.init._res_id, opt);
-          });
-
-          options = $.extend({}, $.mws.defaults, options);
-
-          if (options.height){ this.height(options.height); }
-          if (options.width){ this.width(options.width); }
-
-          return this;
-        default:
-          throw new TypeError('Parameter must be a string or options object');
-        }
-      }
-    });
-
-    $.mws = {
-      defaults: {
-        create_new: true,
-        init_data: true,
-        init_url: undefined,
-        init_json: undefined,
-        height: undefined,
-        width: undefined
-      },
-
-      setDefaults: function(options){
-        return $.extend($.mws.defaults, options);
-      }
-    };
-  };
-
   return {
     run: run,
     runInitializationScripts: runInitializationScripts,
-    jQuery: jQueryInit,
     _initState: initState,
     _lockShells: lockShells,
     _unlockShells: unlockShells,
     _initShell: initShell,
-    _res_id: res_id
+    res_id: res_id
   };
 })();
