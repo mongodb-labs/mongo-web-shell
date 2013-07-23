43a9cdbd9331a642211f3786ce935ed2a97283bc
Ryan Chan <ryan@rcchan.com>
2013-07-23 14:08:59 -0400

    Change mongo.events to use function-return

--- frontend/src/mws/events.js
+++ frontend/src/mws/events.js
@@ -1,23 +1,23 @@
 /* global mongo, console */
 /* jshint noarg: false */
-mongo.events = {
-  trigger: function(shell, event, data){
+mongo.events = (function(){
+  var trigger = function(shell, event, data){
     data = $.extend({shell: shell}, data);
     console.info('[' + shell.id + '] ' + event + ' triggered with data ', data);
     $(shell.$rootElement).trigger('mws:' + event, data);
-  },
+  };
 
-  functionTrigger: function(shell, event, args, data){
+  var functionTrigger = function(shell, event, args, data){
     data = $.extend({'arguments': args}, data);
     mongo.events.trigger(shell, event, data);
-  },
+  };
 
-  callbackTrigger: function(shell, event, result, data){
+  var callbackTrigger = function(shell, event, result, data){
     data = $.extend({result: result}, data);
     mongo.events.trigger(shell, event, data);
-  },
+  };
 
-  bind: function(shell, event, handler, data, filter){
+  var bind = function(shell, event, handler, data, filter){
     return $.Deferred(function(deferred){
       data = $.extend({shell: shell}, data);
       $(shell.$rootElement).bind('mws:' + event, data, function(event){
@@ -30,20 +30,29 @@ mongo.events = {
         deferred.resolveWith(shell, [event, data]);
       });
     }).promise();
-  },
+  };
 
-  bindOnce: function(shell, event, handler, data, filter){
+  var bindOnce = function(shell, event, handler, data, filter){
     data = $.extend({shell: shell}, data);
     var wrappedHandler = function(){
       handler.apply(shell, arguments);
       $(shell.$rootElement).unbind('mws:' + event, arguments.callee.caller);
     };
     return mongo.events.bind(shell, event, wrappedHandler, data, filter);
-  },
+  };
 
-  bindAll: function(event, handler, data, filter){
+  var bindAll = function(event, handler, data, filter){
     return mongo.shells.map(function(e){
       return mongo.events.bind(e, event, handler, data, filter);
     });
-  }
-};
+  };
+
+  return {
+    trigger: trigger,
+    functionTrigger: functionTrigger,
+    callbackTrigger: callbackTrigger,
+    bind: bind,
+    bindOnce: bindOnce,
+    bindAll: bindAll
+  };
+})();
