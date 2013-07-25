8d8a0e8cc6a96326d31ec69cd30ab7cb5d5e778e
Ryan Chan <ryan@rcchan.com>
2013-07-25 11:59:25 -0400

    Option for get_db to raise MWSServerError

--- mongows/mws/db.py
+++ mongows/mws/db.py
@@ -4,11 +4,12 @@ from flask import current_app
 import pymongo
 
 from pymongo.errors import ConnectionFailure, AutoReconnect
+from mongows.mws.MWSServerError import MWSServerError
 
 db = None
 
 
-def get_db():
+def get_db(MWSExceptions=False):
     global db
     # TODO: Ensure MongoClient connection is still active.
     if db:
@@ -16,21 +17,26 @@ def get_db():
     config = urlparse(current_app.config['MONGOHQ_URL'])
     db_name = config.path.rpartition('/')[2]
     try:
-        client = pymongo.MongoClient(config.hostname, config.port)
-    except TypeError:
-        print 'Port is not an instance of int.'
+        try:
+            client = pymongo.MongoClient(config.hostname, config.port)
+        except TypeError:
+            print 'Port is not an instance of int.'
+            raise
+        except ConnectionFailure:
+            print 'Connection to the database could not be made.'
+            raise
+        except AutoReconnect:
+            print 'Auto-reconnection performed.'
+            raise
+        except:
+            print 'An unexpected error occurred.'
+            raise
+        else:
+            db = client[db_name]
+            if config.username:
+                db.authenticate(config.username, config.password)
+            return db
+    except Exception as e:
+        if MWSExceptions:
+            raise MWSServerError(500, str(e))
         raise
-    except ConnectionFailure:
-        print 'Connection to the database could not be made.'
-        raise
-    except AutoReconnect:
-        print 'Auto-reconnection performed.'
-        raise
-    except:
-        print 'An unexpected error occurred.'
-        raise
-    else:
-        db = client[db_name]
-        if config.username:
-            db.authenticate(config.username, config.password)
-        return db
