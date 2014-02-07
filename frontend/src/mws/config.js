5589fdbd12d2e526b4308190208245e5e72d2b2c
idbentley <ian.bentley@gmail.com>
2014-02-07 12:03:55 -0500

    change baseUrl

--- frontend/src/mws/config.js
+++ frontend/src/mws/config.js
@@ -33,6 +33,6 @@ mongo.config = (function () {
     shellHistoryKey: 'mongo.history',
     shellHistorySize: 500,
     mwsHost: mwsHost,
-    baseUrl: mwsHost + '/mws/'
+    baseUrl: mwsHost + '/server/mws/'
   };
 }());
